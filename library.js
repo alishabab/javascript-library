0682a31e3c603c4460f93a5f84789ec2f3fcba28
Ikraam <ikraamghoor@gmail.com>
2020-07-14 12:23:56 +0200

    Refactored to ES6 class and improved functions.

--- library.js
+++ library.js
@@ -1,87 +1,123 @@
-const myLibrary = []
+const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  // this.info = function () {
-  //   return `The ${title} by ${author}, ${pages} pages, ${!read ? 'not read yet' : 'read already'}`;
-  // };
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+
+    this.author = author;
+
+    this.pages = pages;
+
+    this.read = read;
+  }
+
+  isRead(render) {
+    this.read = !this.read;
+    render();
+  }
+
+  static deleteBook(index, render) {
+    myLibrary.splice(index, 1);
+    render();
+  }
 }
 
-Book.prototype.isRead =  function(book,cb) {
-  return function() {
-  book.read = !book.read;
-  cb()
-  }
+const clearElement = (element) => {
+  element.innerHTML = '';
 };
 
-let cbutton;
-const addBookBtn = document.querySelector('#addBookBtn');
 function addBookToLibrary() {
   const title = document.querySelector('#title').value;
+
   const author = document.querySelector('#author').value;
+
   const pages = document.querySelector('#pages').value;
+
   const read = document.querySelector('#read').checked;
+
   const newbook = new Book(title, author, pages, read);
+
   myLibrary.push(newbook);
+
+  clearElement(document.querySelector('#form'));
+  // eslint-disable-next-line no-use-before-define
   render();
 }
 
 const renderForm = () => {
   const form = document.querySelector('#form');
+
   form.innerHTML = `<label for="title">Title</label>
+
   <input type="text" name="title" id="title" />
+
   <label for="author">author</label>
+
   <input type="text" name="author" id="author" />
+
   <label for="pages">pages</label>
+
   <input type="text" name="pages" id="pages" />
+
   <label for="read">read</label>
+
   <input type="checkbox" name="read" id="read" />
+
   <button type="submit" id= 'create'>Create Book</button>`;
-  cbutton = document.querySelector('#create');
-  cbutton.addEventListener('click', addBookToLibrary);
+
+  document.querySelector('#create').addEventListener('click', addBookToLibrary);
 };
-
-
-addBookBtn.addEventListener('click', renderForm);
-
-function deleteBook(index) {
-  return function () {
-    myLibrary.splice(index, 1);
-    render();
-  };
-}
 
 function render() {
   const div = document.querySelector('.work');
-  div.innerHTML = '';
+
+  clearElement(div);
+  // eslint-disable-next-line no-use-before-define
+  renderBooks(div);
+}
+
+const renderBooks = (div) => {
   myLibrary.forEach((book, index) => {
     const ul = document.createElement('ul');
-    bookArray = Object.entries(book)
-    for (const i in bookArray ) {
+
+    const bookArray = Object.entries(book);
+
+    bookArray.forEach((book, i) => {
       const li = document.createElement('li');
+
       li.innerText = `${bookArray[i][0]}-${bookArray[i][1]}`;
+
       ul.appendChild(li);
-    }
+    });
+
     const toggle = document.createElement('button');
-    toggle.innerText = `${!book.read?'Read':'Unread'}`
+
+    toggle.innerText = `${!book.read ? 'Read' : 'Unread'}`;
+
     toggle.setAttribute('type', 'submit');
+
     ul.appendChild(toggle);
-    const dltb = document.createElement('button');
-    dltb.innerText = `Delete book${index}`;
-    dltb.setAttribute('id', `book${index}`);
-    ul.appendChild(dltb);
+
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.innerText = `Delete book${index}`;
+
+    deleteBtn.setAttribute('id', `book${index}`);
+
+    ul.appendChild(deleteBtn);
+
     div.appendChild(ul);
-    dltb.addEventListener('click', deleteBook(index));
-    toggle.addEventListener('click', book.isRead(book,render));
+
+    deleteBtn.addEventListener('click', () => { Book.deleteBook(index, render); });
+
+    toggle.addEventListener('click', () => { book.isRead(render); });
   });
-}
+};
 
 
 const book1 = new Book('Hobbit', 'J.R.R. Tolkien', 295, true);
+
 myLibrary.push(book1);
+document.querySelector('#addBookBtn').addEventListener('click', renderForm);
 render();
-/*eslint linebreak-style: ["error", "windows"]*/
-// console.log(book1.info());
